8f61bcd33ec86e732cf833d0d12392f0881df845
Daniel <dandany123@gmail.com>
2026-01-28 18:14:25 +0200

    added student details page

---
+++ app/src/main/java/com/ds/studentapp/StudentDetailsActivity.kt
@@ -0,0 +1,50 @@
+package com.ds.studentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ds.studentapp.model.Model
+
+class StudentDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_student_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        intent.getStringExtra("studentId")?.let { studentId ->
+            loadStudentDetails(studentId)
+
+            findViewById<Button>(R.id.edit_button).setOnClickListener {
+            }
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        intent.getStringExtra("studentId")?.let { studentId ->
+            loadStudentDetails(studentId)
+        }
+    }
+
+    private fun loadStudentDetails(studentId: String) {
+        Model.getInstance().getStudentById(studentId)?.let {
+            findViewById<TextView>(R.id.name_text).text = it.name
+            findViewById<TextView>(R.id.id_text).text = it.id
+            findViewById<TextView>(R.id.phone_text).text = it.phone
+            findViewById<TextView>(R.id.address_text).text = it.address
+            findViewById<CheckBox>(R.id.check).isChecked = it.checked
+        } ?: run {
+            finish()
+        }
+    }
+}
